2e9a6631f799b5471bfc74191d4780fa9a34da05
AHua <prime73num@163.com>
2024-08-25 11:37:46 +0800

    init java project structrue

---
+++ gulimall/gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,14 @@
+package com.atguigu.gulimall.ware;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GulimallWareApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("hello world");
+    }
+
+}
